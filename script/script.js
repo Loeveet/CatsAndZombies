57e1707fad4ba5a8d2b75ad1e82c4ca169dee9b4
Robin <loeveet@gmail.com>
2023-04-11 22:37:04 +0200

    added grid and buttons

--- script/script.js
+++ script/script.js
@@ -1,40 +1,87 @@
+let rowmove = 4
+let colmove = 2
 drawtable()
-function drawtable(){
-    // let table = ""
-    // table += "<table>"
+createbuttons()
 
-    // for (let i = 0; i < 5; i++) {
-    //     table += "<tr>"
+function drawtable() {
 
-    //     for (let j = 0; j < 5; j++) {
-    //         table += "<td>" + " " + "</td>"
-
-    //     }
-    //     table += "</tr>"
-
-
-    // }
-
-
-    // table += "</table>"
-
-    // document.getElementById("field").innerHTML = table
-    
-    // Skapa en tabell och lägg till den i dokumentet
-    let table = document.createElement('table');
     let field = document.getElementById("field")
-    document.body.appendChild(table);
-    
-    // Skapa rader och celler
-    for (var i = 0; i < 5; i++) {
-        var row = table.insertRow();
-        for (var j = 0; j < 5; j++) {
-            var cell = row.insertCell();
-            cell.innerHTML = 'Cell ' + (i * 5 + j + 1);
+    field.innerHTML = ""
+
+    let table = document.createElement("table");
+
+    for (let i = 0; i < 5; i++) {
+
+        let row = table.insertRow();
+
+        for (let j = 0; j < 5; j++) {
+
+            let cell = row.insertCell()
+
+            cell.innerHTML = i == rowmove && j == colmove ? "X" : " "
+
         }
     }
 
     field.appendChild(table)
 }
+
+function createbuttons() {
+
+    let buttons = document.getElementById("buttons")
+
+    let buttonup = document.createElement("input")
+    buttonup.setAttribute("type", "button")
+    buttonup.setAttribute("value", "Upp")
+    buttonup.setAttribute("id", "buttonup")
+
+    let buttondown = document.createElement("input")
+    buttondown.setAttribute("type", "button")
+    buttondown.setAttribute("value", "Ner")
+    buttondown.setAttribute("id", "buttondown")
+
+    let buttonright = document.createElement("input")
+    buttonright.setAttribute("type", "button")
+    buttonright.setAttribute("value", "Höger")
+    buttonright.setAttribute("id", "buttonright")
+    let buttonleft = document.createElement("input")
+    buttonleft.setAttribute("type", "button")
+    buttonleft.setAttribute("value", "Vänster")
+    buttonleft.setAttribute("id", "buttonleft")
+
+
+    buttons.appendChild(buttonup)
+    buttons.appendChild(buttondown)
+    buttons.appendChild(buttonleft)
+    buttons.appendChild(buttonright)
+
+
+}
+
+function myFunction(value) {
+    console.log(value);
+    console.log(rowmove + " " + colmove)
+    switch (value) {
+        case 1:
+            if (rowmove != 0) rowmove--
+            break;
+        case 2:
+            if (rowmove != 4) rowmove++
+            break;
+        case 3:
+            if (colmove != 0) colmove--
+            break;
+        case 4:
+            if (colmove != 4) colmove++
+            break;
+    }
+    drawtable()
+}
+
+// document.getElementById("buttonup").addEventListener("mouseover", function () { myFunction(1); }); ROLIG FUNKTION
+document.getElementById("buttonup").addEventListener("click", function () { myFunction(1); });
+document.getElementById("buttondown").addEventListener("click", function () { myFunction(2); });
+document.getElementById("buttonleft").addEventListener("click", function () { myFunction(3); });
+document.getElementById("buttonright").addEventListener("click", function () { myFunction(4); });
 
 
