c6277bfed80967f592d2b9eb7bb9035c5985c226
Robin <loeveet@gmail.com>
2023-04-13 09:02:27 +0200

    added pictures and logic

--- script/script.js
+++ script/script.js
@@ -1,10 +1,37 @@
-let rowmove = 4
-let colmove = 2
+let posX = 2
+let posY = 2
+let catX = rndInt(0, 5)
+let catY = rndInt(0, 5)
+let zomX = rndInt(0, 5)
+let zomY = rndInt(0, 5)
+let catcount = 0
+
+let foundcat = false
+let dead = false
+
+let images = [
+    { image: "images/cabinbylake.jpg", value: "Från stugan kan man dyka ner i sjön" },
+    { image: "images/reef.jpg", value: "Fiskarna har möte vid revet" },
+    { image: "images/skog.jpg", value: "Solen skiner genom gläntan i skogen" },
+    { image: "images/asianhouse.jpg", value: "Vackert hus i skogen" },
+    { image: "images/lakeandsnowymontains.avif", value: "Vackra berg bakom en spegelsjö" },
+    { image: "images/oldcabininthewoods.jpg", value: "Sliten stuga i morgonljuset" },
+    { image: "images/pinktree.jpg", value: "Blommande körsbärsträd" },
+    { image: "images/stream.jpg", value: "Strömmande vatten" },
+    { image: "images/sunsetandbirds.jpg", value: "Fåglarna flyger tillsammans i solnedgången" },
+    { image: "images/valley.jpg", value: "Valley" },
+    { image: "images/waterfall.jpg", value: "Waterfall" },
+    { image: "images/waterfallandmountain.jpg", value: "Waterfall and mountain" },
+    { image: "images/wave.jpg", value: "En våg perfekt att surfa på" }];
+    
+    const url = "https://api.chucknorris.io/jokes/random/"
+const theJoke = document.getElementById("joke")
+
+
+printScore()
+showimage()
 drawtable()
 createbuttons()
-
-const url = "https://api.chucknorris.io/jokes/random/"
-const theJoke = document.getElementById("joke")
 
 fetch(url)
     .then(function (response) { return response.json() })
@@ -19,13 +46,6 @@ fetch(url)
         value.setAttribute("class", "value")
         value.innerHTML = data.value
 
-        // Bilden verkar inte finnas
-        // let img = document.createElement("img")
-        // img.setAttribute("alt", "Image not found")
-        // img.src = data.icon_url
-
-        // card.appendChild(img)
-        
         card.appendChild(value)
         theJoke.appendChild(card)
 
@@ -34,20 +54,53 @@ fetch(url)
 
 function drawtable() {
 
+    console.log("Spelare: " + posX + " " + posY)
+    console.log("Kissekatten: " + catX + " " + catY)
+    console.log("Zombie: " + zomX + " " + zomY)
+    console.log("Counter: " + catcount)
+
+
     let field = document.getElementById("field")
     field.innerHTML = ""
+    let matrix = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25]
+    ];
 
     let table = document.createElement("table");
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < matrix.length; i++) {
 
         let row = table.insertRow();
 
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < matrix[i].length; j++) {
 
             let cell = row.insertCell()
 
-            cell.innerHTML = i == rowmove && j == colmove ? "X" : " "
+            if (i == posX && j == posY) {
+                cell.innerHTML = "X"
+            }
+            else if (i == catX && j == catY) {
+                cell.innerHTML = "C"
+            }
+            else if (i == zomX && j == zomY) {
+                cell.innerHTML = "Z"
+            }
+            else {
+                cell.innerHTML = " "
+            }
+            // cell.innerHTML = i == posX && j == posY ? "X" : " "
+            // cell.innerHTML = i == catX && j == catY ? "C" : " "
+            // cell.innerHTML = i == zomX && j == zomY ? "Z" : " "
+            // if(posX == catX && posY == catY) {
+            //     foundcat = true
+            //     counter++
+            // }
+            posX == catX && posY == catY ? foundcat = true : foundcat = false
+            posX == zomX && posY == zomY ? dead = true : dead = false
 
         }
     }
@@ -55,61 +108,215 @@ function drawtable() {
     field.appendChild(table)
 }
 
+function printScore() {
+    let score = document.getElementById("score")
+    score.innerHTML = ""
+
+    let currentscore = document.createElement("h4")
+    currentscore.setAttribute("id", "currentscore")
+    currentscore.innerHTML = "Hittade katter: " + catcount
+
+    score.appendChild(currentscore)
+}
+
+function printDead() {
+    let text = document.getElementById("currentscore")
+    text.innerHTML = "Zombien tog dig! MEN du hann hitta " + catcount + " katter"
+    catcount = 0
+
+
+}
+
 function createbuttons() {
 
-    let buttons = document.getElementById("buttons")
+    let buttons = document.getElementById("button-container")
 
+    let row1 = document.createElement("div")
+    row1.setAttribute("class", "buttonsrow1")
     let buttonup = document.createElement("input")
     buttonup.setAttribute("type", "button")
     buttonup.setAttribute("value", "Upp")
     buttonup.setAttribute("id", "buttonup")
+    buttonup.setAttribute("class", "button")
+    row1.appendChild(buttonup);
 
-    let buttondown = document.createElement("input")
-    buttondown.setAttribute("type", "button")
-    buttondown.setAttribute("value", "Ner")
-    buttondown.setAttribute("id", "buttondown")
-
-    let buttonright = document.createElement("input")
-    buttonright.setAttribute("type", "button")
-    buttonright.setAttribute("value", "Höger")
-    buttonright.setAttribute("id", "buttonright")
+    
+    let row2 = document.createElement("div")
+    row2.setAttribute("class", "buttonsrow2")
     let buttonleft = document.createElement("input")
     buttonleft.setAttribute("type", "button")
     buttonleft.setAttribute("value", "Vänster")
     buttonleft.setAttribute("id", "buttonleft")
+    buttonleft.setAttribute("class", "button")
+    
+    let buttonright = document.createElement("input")
+    buttonright.setAttribute("type", "button")
+    buttonright.setAttribute("value", "Höger")
+    buttonright.setAttribute("id", "buttonright")
+    buttonright.setAttribute("class", "button")
+    
+    row2.appendChild(buttonleft)
+    row2.appendChild(buttonright)
 
+    let row3 = document.createElement("div")
+    row3.setAttribute("class", "buttonsrow3")
+    let buttondown = document.createElement("input")
+    buttondown.setAttribute("type", "button")
+    buttondown.setAttribute("value", "Ner")
+    buttondown.setAttribute("id", "buttondown")
+    buttondown.setAttribute("class", "button")
+    
+    row3.appendChild(buttondown)
 
-    buttons.appendChild(buttonup)
-    buttons.appendChild(buttondown)
-    buttons.appendChild(buttonleft)
-    buttons.appendChild(buttonright)
+    buttons.appendChild(row1)
+    buttons.appendChild(row2)
+    buttons.appendChild(row3)
 
 
 }
 
-function myFunction(value) {
-    console.log(value);
-    console.log(rowmove + " " + colmove)
+function changePosition(value) {
     switch (value) {
         case 1:
-            if (rowmove != 0) rowmove--
+            if (posX != 0) {
+                posX--
+                drawtable()
+                showimage()
+            }
             break;
         case 2:
-            if (rowmove != 4) rowmove++
+            if (posX != 4) {
+                posX++
+                drawtable()
+                showimage()
+            }
             break;
         case 3:
-            if (colmove != 0) colmove--
+            if (posY != 0) {
+                posY--
+                drawtable()
+                showimage()
+            }
             break;
         case 4:
-            if (colmove != 4) colmove++
+            if (posY != 4) {
+                posY++
+                drawtable()
+                showimage()
+            }
             break;
     }
-    drawtable()
+
 }
 
 // document.getElementById("buttonup").addEventListener("mouseover", function () { myFunction(1); }); ROLIG FUNKTION
-document.getElementById("buttonup").addEventListener("click", function () { myFunction(1); });
-document.getElementById("buttondown").addEventListener("click", function () { myFunction(2); });
-document.getElementById("buttonleft").addEventListener("click", function () { myFunction(3); });
-document.getElementById("buttonright").addEventListener("click", function () { myFunction(4); });
+document.getElementById("buttonup").addEventListener("click", function () { changePosition(1); });
+document.getElementById("buttondown").addEventListener("click", function () { changePosition(2); });
+document.getElementById("buttonleft").addEventListener("click", function () { changePosition(3); });
+document.getElementById("buttonright").addEventListener("click", function () { changePosition(4); });
 
+function rndInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+function showimage() {
+    let rndNum = rndInt(0, images.length)
+
+    let image = document.getElementById("image")
+    image.innerHTML = ""
+
+    let bigimage = document.createElement("img")
+    bigimage.src = images[rndNum].image
+    bigimage.setAttribute("class", "image")
+
+    let bigimagetext = document.createElement("p")
+    bigimagetext.innerHTML = images[rndNum].value
+
+    let catimage = document.createElement("img")
+    catimage.src = "images/gustav.png"
+    catimage.setAttribute("class", "catimage")
+
+    let zombieimage = document.createElement("img")
+    zombieimage.src = "images/zombie.png"
+    zombieimage.setAttribute("class", "zombieimage")
+
+    image.appendChild(bigimage)
+    if (foundcat == true) {
+        catcount++
+        printScore()
+        image.appendChild(catimage)
+        catX = rndInt(0, 5)
+        catY = rndInt(0, 5)
+        foundcat = false
+        drawtable()
+    }
+    else if (dead == true) {
+        image.appendChild(zombieimage)
+        printDead()
+        zomX = rndInt(0, 5)
+        zomY = rndInt(0, 5)
+        dead = false
+        drawtable()
+    }
+    image.appendChild(bigimagetext)
+
+}
+
+
+
+
+//Bjudkoder från bing här under. För att flytta zombien. Lägg till imorgon
+
+// Flytta fienden närmare markören
+if (Math.abs(enemyRow - markerRow) > Math.abs(enemyCol - markerCol)) {
+    // Flytta fienden i y-led
+    if (enemyRow < markerRow) {
+        enemyRow++;
+    } else if (enemyRow > markerRow) {
+        enemyRow--;
+    }
+} else {
+    // Flytta fienden i x-led
+    if (enemyCol < markerCol) {
+        enemyCol++;
+    } else if (enemyCol > markerCol) {
+        enemyCol--;
+    }
+}
+
+let markerRow = 0; // Markörens radposition
+let markerCol = 0; // Markörens kolumnposition
+let enemyRow = 5; // Fiendens radposition
+let enemyCol = 5; // Fiendens kolumnposition
+
+function moveMarker(direction) {
+    // Uppdatera markörens position beroende på riktning
+    if (direction === "up") {
+        markerRow--;
+    } else if (direction === "down") {
+        markerRow++;
+    } else if (direction === "left") {
+        markerCol--;
+    } else if (direction === "right") {
+        markerCol++;
+    }
+
+    // Rita om markören på dess nya position
+    drawMarker(markerRow, markerCol);
+
+    // Flytta fienden närmare markören
+    if (enemyRow < markerRow) {
+        enemyRow++;
+    } else if (enemyRow > markerRow) {
+        enemyRow--;
+    }
+    if (enemyCol < markerCol) {
+        enemyCol++;
+    } else if (enemyCol > markerCol) {
+        enemyCol--;
+    }
+
+    // Rita om fienden på dess nya position
+    drawEnemy(enemyRow, enemyCol);
+}
